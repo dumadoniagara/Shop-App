3bfbb407e10cc092d0cfb4fb1612be9ceee5d879
Duma Doniagara <duma@dusdusan.com>
2021-11-16 16:17:28 +0700

    listing and detail

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { AppLoading } from "expo";
+import { useFonts } from 'expo-font';
+
+import ShopNavigator from './navigation/shopNavigator';
+import productsReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+})
+
+const store = createStore(rootReducer);
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
   );
+
 }
 
 const styles = StyleSheet.create({
---
+++ data/dummy-data.js
@@ -0,0 +1,62 @@
+import Product from '../models/product';
+
+const PRODUCTS = [
+    new Product(
+        'p1',
+        'u1',
+        'Red Shirt',
+        'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+        'A red t-shirt, perfect for days with non-red weather.',
+        29.99
+    ),
+    new Product(
+        'p2',
+        'u1',
+        'Blue Carpet',
+        'https://images.pexels.com/photos/6292/blue-pattern-texture-macro.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'Fits your red shirt perfectly. To stand on. Not to wear it.',
+        99.99
+    ),
+    new Product(
+        'p3',
+        'u2',
+        'Coffee Mug',
+        'https://images.pexels.com/photos/160834/coffee-cup-and-saucer-black-coffee-loose-coffee-beans-160834.jpeg?cs=srgb&dl=bean-beans-black-coffee-160834.jpg&fm=jpg',
+        'Can also be used for tea!',
+        8.99
+    ),
+    new Product(
+        'p4',
+        'u3',
+        'The Book - Limited Edition',
+        'https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?cs=srgb&dl=blur-blurred-book-pages-46274.jpg&fm=jpg',
+        "What the content is? Why would that matter? It's a limited edition!",
+        15.99
+    ),
+    new Product(
+        'p5',
+        'u3',
+        'PowerBook',
+        'https://get.pxhere.com/photo/laptop-computer-macbook-mac-screen-water-board-keyboard-technology-air-mouse-photo-airport-aircraft-tablet-aviation-office-black-monitor-keys-graphic-hardware-image-pc-exhibition-multimedia-calculator-vector-water-cooling-floppy-disk-phased-out-desktop-computer-netbook-personal-computer-computer-monitor-electronic-device-computer-hardware-display-device-448748.jpg',
+        'Awesome hardware, crappy keyboard and a hefty price. Buy now before a new one is released!',
+        2299.99
+    ),
+    new Product(
+        'p6',
+        'u1',
+        'Pen & Paper',
+        'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
+        "Can be used for role-playing (not the kind of role-playing you're thinking about...).",
+        5.49
+    ),
+    new Product(
+        'p7',
+        'u1',
+        'Dispenser Decorated',
+        'https://images.dusdusan.com/product/OaVZN0zT1636604092_HEREVIN - DISPENSER DECORATED SQUARE-04.jpg',
+        'Quam elementum pulvinar etiam non quam lacus suspendisse. Consectetur libero id faucibus nisl. Id porta nibh venenatis cras sed felis eget. Dui accumsan sit amet nulla facilisi morbi. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Facilisi nullam vehicula ipsum a arcu. Ornare aenean euismod elementum nisi quis eleifend. Velit laoreet id donec ultrices tincidunt. Orci eu lobortis elementum nibh tellus molestie nunc non. Tincidunt dui ut ornare lectus sit amet est placerat in. Amet nisl purus in mollis nunc sed id semper. Nunc sed id semper risus. Cursus turpis massa tincidunt dui ut. Sit amet tellus cras adipiscing. Amet nisl suscipit adipiscing bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Sit amet venenatis urna cursus eget nunc scelerisque viverra mauris. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Gravida dictum fusce ut placerat orci nulla pellentesque dignissim. Vitae elementum curabitur vitae nunc sed velit. Quam elementum pulvinar etiam non quam lacus suspendisse. Consectetur libero id faucibus nisl. Id porta nibh venenatis cras sed felis eget. Dui accumsan sit amet nulla facilisi morbi. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Facilisi nullam vehicula ipsum a arcu. Ornare aenean euismod elementum nisi quis eleifend. Velit laoreet id donec ultrices tincidunt. Orci eu lobortis elementum nibh tellus molestie nunc non. Tincidunt dui ut ornare lectus sit amet est placerat in. Amet nisl purus in mollis nunc sed id semper. Nunc sed id semper risus. Cursus turpis massa tincidunt dui ut. Sit amet tellus cras adipiscing. Amet nisl suscipit adipiscing bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Sit amet venenatis urna cursus eget nunc scelerisque viverra mauris. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Gravida dictum fusce ut placerat orci nulla pellentesque dignissim. Vitae elementum curabitur vitae nunc sed velit.',
+        19.99
+    ),
+];
+
+export default PRODUCTS;
